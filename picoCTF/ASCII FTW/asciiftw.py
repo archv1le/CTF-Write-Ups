2add06f36a981bdb6080aad691a098cc0a2a3e8c
archv1le <158765690+archv1le@users.noreply.github.com>
2024-02-12 22:56:13 +0600

    Create asciiftw.py

---
+++ picoCTF/ASCII FTW/asciiftw.py
@@ -0,0 +1,20 @@
+from ghidra.program.model.mem import MemoryAccessException
+
+start_addr = currentProgram.getAddressFactory().getAddress("00101184")
+end_addr = currentProgram.getAddressFactory().getAddress("001011fc")
+
+byte_list = []
+
+current_addr = start_addr
+while current_addr <= end_addr:
+	instruction = getInstructionAt(current_addr)
+	if instruction is not None:
+		
+		operand = instruction.getOpObjects(1)[0] # get second operand
+		byte_val = operand.getValue() & 0xFF # convert to unsigned byte
+		byte_list.append(byte_val)
+                
+        current_addr = current_addr.next()
+
+flag = list(map(chr, byte_list))
+print("Flag:", "".join(flag))
